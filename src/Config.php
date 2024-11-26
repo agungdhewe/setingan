eeeea42fa006236325a537d6a9e7a916c607a677
agung-transfashion <>
2024-11-26 10:07:22 +0700

    dev 20241126 1007

--- src/Config.php
+++ src/Config.php
@@ -10,18 +10,71 @@ class Config
 	private static array $_usedConfig;
 	private static string $_rootDir;
 
+	/**
+	 * Sets the root directory of the application.
+	 *
+	 * If the `__ROOT_DIR__` constant is not already defined, it will be defined with the provided directory.
+	 *
+	 * @param string $dir The root directory of the application.
+	 *
+	 * @return void
+	 */
+	public static function SetRootDir($dir) : void {
+		if (!defined('__ROOT_DIR__')) {
+			define('__ROOT_DIR__', $dir);
+		}
 
+		self::$_rootDir = $dir;
+	}
+
+	/**
+	 * Gets the root directory of the application.
+	 *
+	 * @return string The root directory of the application.
+	 */
+	public static function GetRootDir() : string {
+		return self::$_rootDir;
+	}
+
+
+	/**
+	 * Sets up the configuration for the application.
+	 *
+	 * This function is used to initialize the configuration array with the provided values.
+	 *
+	 * @param array $config An associative array containing the configuration values.
+	 *
+	 * @return void
+	 */
 	public static function Setup(array $config) : void {
 		self::$_config = $config;
 	}
 
 
+	
+	/**
+	 * Sets a configuration value.
+	 *
+	 * @param string $key   The key of the configuration value.
+	 * @param mixed  $value The value to set.
+	 *
+	 * @return void
+	 */
 	public static function Set(string $keypath, mixed $value) : void {
 		if (is_array(self::$_config)) {
 			//TODO: masukkan keypath dengan value ke $_config 	
 		}
 	}
 
+
+	/**
+	 * Gets a configuration value.
+	 *
+	 * @param string $key     The key of the configuration value.
+	 * @param mixed  $default The default value to return if the key is not set.
+	 *
+	 * @return mixed The configuration value, or the default value if not set.
+	 */
 	public static function Get(?string $keypath = null) : mixed {
 		try {
 			if ($keypath!==null) {
@@ -35,7 +88,15 @@ class Config
 		}
 	}
 
-
+	/**
+	 * mark a key as a reference for used configuration.
+	 *
+	 * @param string $name The name of the user configuration value.
+	 *
+	 * @throws \Exception If the user configuration value is not found.
+	 *
+	 * @return string The user configuration value.
+	 */
 	public static function GetUsedConfig(string $name) : string {
 		if (!array_key_exists($name, self::$_usedConfig)) {
 			throw new \Exception("Config '$name' tidak ditemukan");
@@ -43,9 +104,19 @@ class Config
 		return self::$_usedConfig[$name];
 	}
 
+	/**
+	 * Sets the used configuration key values.
+	 *
+	 * @param array $usedconfig An associative array containing the user configuration values.
+	 *
+	 * @return void
+	 */
 	public static function UseConfig(array $usedconfig) : void {
 		self::$_usedConfig = $usedconfig;
 	}
+
+
+
 
 	private static function getValueByPath(array $array, string $path, ?string $separator = self::SPARATOR) : mixed {
 		$keys = explode($separator, $path);
@@ -59,16 +130,6 @@ class Config
 	}
 
 
-	public static function setRootDir($dir) : void {
-		if (!defined('__ROOT_DIR__')) {
-			define('__ROOT_DIR__', $dir);
-		}
 
-		self::$_rootDir = $dir;
-	}
-
-	public static function getRootDir() : string {
-		return self::$_rootDir;
-	}
 
 }
